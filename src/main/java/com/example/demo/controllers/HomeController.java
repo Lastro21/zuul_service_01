a591a5632fe69d377ff769821b95aaaf9ccb38e0
Lastro21 <ptokarev8@gmail.com>
2018-12-30 15:54:52 +0300

    zuul_service01

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+final public class HomeController {
+
+    @Value("${my_any_app.my_any_id}")
+    String instance;
+
+    @GetMapping("/any")
+    public String hi() throws InterruptedException {
+
+        //Thread.sleep(900);
+
+        return "Welcome, i am " + instance;
+
+    }
+}
